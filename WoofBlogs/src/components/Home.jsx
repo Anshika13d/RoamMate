ea82565c7dc59cb28651c286d97001cf7d791f71
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2024-03-19 13:20:27 +0530

    made card on day 2

---
+++ WoofBlogs/src/components/Home.jsx
@@ -0,0 +1,68 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom'
+import Card from 'react-bootstrap/Card';
+
+
+function Home({data}) {
+  
+  return (
+    <>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={data.dog1.img} />
+      <Card.Body>
+        <Card.Title>{data.dog1.name } ({data.dog1.age})</Card.Title>
+        <Card.Text>
+          {data.dog1.des}
+        </Card.Text>
+        <Button variant="primary">Explore {data.dog1.name}</Button>
+      </Card.Body>
+    </Card>
+    </>
+  );
+}
+
+export default Home;
