2951a36385e8fe1bbc78b109e1e236e68bae992b
Anshika <anshika1366@gmail.com>
2024-03-20 12:52:24 +0530

    added cards

--- WoofBlogs/src/App.jsx
+++ WoofBlogs/src/App.jsx
@@ -1,28 +1,19 @@
-<<<<<<< Updated upstream
-=======
+
 import Home from './components/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> Stashed changes
+
+
 
 function App() {
-  let data = {
-    
-    dog1:{
-      name:"dollar",
-      img: "https://images.pexels.com/photos/8473445/pexels-photo-8473445.jpeg?auto=compress&cs=tinysrgb&w=800",
-      age: 4,
-      des:"This dog is the pet of ujjwal shrivastava"
-    },
-
-  }
+  
 
   return (
     <>
-<<<<<<< Updated upstream
-      <h1>hello</h1>
-=======
-      <Home  data={data}/>
->>>>>>> Stashed changes
+    {data.map((e, ind) => {
+      return (
+        <Home key={ind} data={e} />
+      )
+    })}
     </>
   )
 }
--- WoofBlogs/src/components/Home.jsx
+++ WoofBlogs/src/components/Home.jsx
@@ -1,12 +1,10 @@
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
-
 
 function Home({data}) {
   
@@ -17,27 +15,23 @@ function Home({data}) {
         <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
+          <ul className="navbar-nav me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}>
+            <li className="nav-item">
+              <NavLink exact to="/" className="nav-link">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/link" className="nav-link">Link</NavLink>
+            </li>
             <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
+              <NavDropdown.Item href="Action">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
+            <li className="nav-item">
+              <NavLink to="#" className="nav-link" disabled>Link</NavLink>
+            </li>
+          </ul>
           <Form className="d-flex">
             <Form.Control
               type="search"
@@ -52,13 +46,13 @@ function Home({data}) {
     </Navbar>
 
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={data.dog1.img} />
+      <Card.Img variant="top" src={data.img} />
       <Card.Body>
-        <Card.Title>{data.dog1.name } ({data.dog1.age})</Card.Title>
+        <Card.Title>{data.name } ({data.age})</Card.Title>
         <Card.Text>
-          {data.dog1.des}
+          {data.des}
         </Card.Text>
-        <Button variant="primary">Explore {data.dog1.name}</Button>
+        <Button variant="primary">Explore {data.name}</Button>
       </Card.Body>
     </Card>
     </>
